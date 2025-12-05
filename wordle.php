ced18870f8189a83e7fbc77e6fe97a47e3f242fb
Joe Robertazzi <robertazzijoseph@gmail.com>
2025-12-05 11:00:29 -0800

    php test

--- wordle.php
+++ wordle.php
@@ -1,7 +1,21 @@
 <?php
-header("Content-Type: application/json");
-
-$date = date("Y-m-d");
+$date = date('Y-m-d');
 $url = "https://www.nytimes.com/svc/wordle/v2/$date.json";
 
-echo file_get_contents($url);
+$json = @file_get_contents($url);
+$solution = "ERROR";
+
+if ($json) {
+    $data = json_decode($json, true);
+    if (isset($data['solution'])) {
+        $solution = strtoupper($data['solution']);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <pre><?php echo $solution; ?></pre>
+</body>
+</html>
